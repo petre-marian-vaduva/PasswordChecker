8f7829552c0217d9dc0072412216079dab298016
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-16 23:34:37 +0100

    add new variables

--- password.js
+++ password.js
@@ -1,6 +1,11 @@
 const password = 'katakana';
 let response;
+let entryCount = 0;
+const entryLimit = 3;
+let error = false;
+
 while(response != password){
      response = window.prompt('Input your password!')
 }
 alert('Youve got it!')
+
